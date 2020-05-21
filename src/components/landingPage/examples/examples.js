0d43b91121e693c09d7ffe520140af9b7751b695
Timur Yermakou <darkday3@mail.ru>
2020-05-21 11:53:42 +0300

    feat: add new examples

--- src/components/landingPage/examples/examples.js
+++ src/components/landingPage/examples/examples.js
@@ -6,7 +6,9 @@ const examples = [
   {img: 'assets/theyalow.jpg', heading: 'TheYalow', text: 'Landing page', link: 'https://zen-gates-d3402f.netlify.com/'},
   {img: 'assets/rdp.jpg', heading: 'Repair-design-project', text: 'Adaptive landing page', link: 'https://objective-aryabhata-b74c8c.netlify.com/'},
   {img: 'assets/fw.jpg', heading: 'Fancy-weather', text: 'Weather application', link: 'https://thirsty-beaver-a2ee9f.netlify.com/'},
-  {img: 'assets/piskel.jpg', heading: 'Piskel-clone', text: 'Application for creating GIF / APNG animations', link: 'https://flamboyant-benz-3b2ef3.netlify.com/'}
+  {img: 'assets/piskel.jpg', heading: 'Piskel-clone', text: 'Application for creating GIF / APNG animations', link: 'https://flamboyant-benz-3b2ef3.netlify.com/'},
+  {img: 'assets/youtube.jpg', heading: 'Simple youtube client', text: 'Simple youtube client(Angular)', link: 'https://friendly-wilson-4fc41d.netlify.app/main'},
+  {img: 'assets/library.jpg', heading: 'Online library', text: 'Online library(Angular)', link: 'https://nostalgic-goldwasser-738e83.netlify.app/login'}
 ];
 
 class Examples extends PureComponent {
